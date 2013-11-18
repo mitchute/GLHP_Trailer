1d12dacedf42f5d4daec2bf2bc3eed4a7d84369e
Matt Mitchell <matt.s.mitchell@okstate.edu>
2013-11-18 09:03:39 -0600

    Compensated for non-linear heater output

--- GLHP_Trailer.ino
+++ GLHP_Trailer.ino
@@ -3,14 +3,11 @@ const int    SCR_Out      = 9;    // Output for SCR
 const int    Q_Set        = 3810; // Q constant variable
 const double R_Static     = 16.1; // Resistance of base heater
 const double R_SCR        = 12.4; // Resistance of variable heater
-const int    SCR_Max      = 118;  // Max value of the SCR 
-const int numReadings     = 30;   //For smoothing values of the inputs
+const int numReadings     = 30;   //For avg values of the inputs
 int start                 = 1;    // For initial value stuff
 int SCR_Out_Set           = 0;
 int DC_Avg                = 0;    //Average RAW Values
 double DC_Voltage, AC_Voltage, SCR_Out_Voltage;  // Raw to Voltage
-double Q_Act              = 0;    //Actual power output
-double deadband           = 35;   //Deadband
 
 //******Initializes the communication and clears the LCD screen.
 void setup() {
@@ -28,7 +25,6 @@ void loop() {
     analogWrite(SCR_Out, SCR_Out_Set);
     start = 0;
   }
-  //Wait_Delay();
   Read_Input(1000);
   Convert_Data();
   Calculate_Data();  
@@ -36,7 +32,7 @@ void loop() {
   Output_to_SCR();      
 }
 
-//******Reads the analog DC voltage*************************************
+//******Reads the analog DC voltage*****************************
 void Read_Input(int Read_Delay) {
   // read the analog in raw value and average them
   int DC_Voltage_Read[numReadings]; 
@@ -78,29 +74,20 @@ void Convert_Data(){
   SCR_Out_Voltage = (SCR_Out_Set * (5.0 / 255)) - 0.6364;
 
   //Convert DC To AC Voltage
-  AC_Voltage = ((DC_Voltage / 4) * 208);
+  AC_Voltage = (8.70e-3 * pow(DC_Voltage,2) + 4.538e1 *
+                  DC_Voltage + 2.379e1);
 }
-
-
 
 void Calculate_Data(){
-
-  double P_SCR = 0; // Percent of SCR
   
-  P_SCR = ((R_SCR / pow(AC_Voltage,2)) * (Q_Set - ((pow(AC_Voltage,2)) / R_Static)));
+  // Base heater calculation
+  double Q_Base = (pow(AC_Voltage,2)/R_Static); 
+  //SCR calculations
+  double Q_SCR = (Q_Set - Q_Base);
   
-  Q_Act =  (pow(AC_Voltage,2)/R_Static) + (pow(AC_Voltage,2)/R_SCR) * P_SCR;
-  
-  if(Q_Act <= (Q_Set + deadband) && (Q_Act >= (Q_Set - deadband)))
-    SCR_Out_Set;
-  else if(Q_Act < Q_Set)
-    SCR_Out_Set ++;
-  else if(Q_Act > Q_Set)
-    SCR_Out_Set --;
-  //SCR_Out_Set = (SCR_Max * P_SCR) + 35;
+  SCR_Out_Set = int(-5.63e-6 * pow(Q_SCR,2) + 5.0e-2 *
+                Q_SCR + 1.23e1);
 }
-
-
 //******Writes analog output D-A to SCR********
 void Output_to_SCR(){
   analogWrite(SCR_Out, SCR_Out_Set);
